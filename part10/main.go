afdcb4593a7b38ec798da1f0610b83a03c149820
kydiaa <jourdainjulien@gmail.com>
2019-07-07 17:01:13 +0200

    [UPDATE] struct name

--- part10/main.go
+++ part10/main.go
@@ -94,7 +94,7 @@ func store_new_token(tokens *[]lexemes, new_token **lexemes) {
 	}
 }
 
-func lexer(expr string) []lexemes {
+func tokenize(expr string) []lexemes {
 	lex := init_lex()
 	var tokens []lexemes
 	var new_token *lexemes
@@ -154,10 +154,14 @@ func prior2(current_token int) bool {
 	Interpreter
 */
 
-type interpreter struct {
+type lexer struct {
 	index int
 	length int
 	tokens []lexemes
+}
+
+type rules struct {
+	lexer lexer
 }
 
 type Element interface {
@@ -225,54 +229,54 @@ type Node struct {
 func (n *Node) Resolve() {
 }
 
-func (i *interpreter) Cur() *lexemes {
-	if i.index == i.length {
+func (l *lexer) Cur() *lexemes {
+	if l.index == l.length {
 		return nil
 	} else {
-		return &i.tokens[i.index]
+		return &l.tokens[l.index]
 	}
 }
 
-func (i *interpreter) Next() *lexemes {
-	i.index++
-	if i.index == i.length {
+func (l *lexer) Next() *lexemes {
+	l.index++
+	if l.index == l.length {
 		return nil
 	} else {
-		return &i.tokens[i.index]
+		return &l.tokens[l.index]
 	}
 }
 
-func (i *interpreter) digest(needed int) {
-	if needed == i.Cur().ttype {
-		fmt.Printf("Digest := [%d] '%s'\n", i.Cur().ttype, i.Cur().tvalue)
-		i.Next()
+func (r *rules) digest(needed int) {
+	if needed == r.lexer.Cur().ttype {
+		fmt.Printf("Digest := [%d] '%s'\n", r.lexer.Cur().ttype, r.lexer.Cur().tvalue)
+		r.lexer.Next()
 	} else {
 		fmt.Fprintf(os.Stderr, "Syntax Error need %d has %d for %s\n",
-			needed, i.Cur().ttype, i.Cur().tvalue)
+			needed, r.lexer.Cur().ttype, r.lexer.Cur().tvalue)
 		os.Exit(-1)
 	}
 }
 
-func (i *interpreter) factor() *Node {
+func (r *rules) factor() *Node {
 	var node *Node
-	token := i.Cur()
+	token := r.lexer.Cur()
 	switch token.ttype {
 	case NUMBER:
-		i.digest(NUMBER)
+		r.digest(NUMBER)
 		node = &Node{nil, token, nil}
 	case WORD:
-		i.digest(WORD)
+		r.digest(WORD)
 		node = &Node{nil, token, nil}
 	case LPAR:
-		i.digest(LPAR)
-		node = i.expr()
-		i.digest(RPAR)
+		r.digest(LPAR)
+		node = r.expr()
+		r.digest(RPAR)
 	case PLUS:
-		i.digest(PLUS)
-		node = &Node{nil, token, i.factor()}
+		r.digest(PLUS)
+		node = &Node{nil, token, r.factor()}
 	case MINUS:
-		i.digest(MINUS)
-		node = &Node{nil, token, i.factor()}
+		r.digest(MINUS)
+		node = &Node{nil, token, r.factor()}
 	default:
 		fmt.Fprintf(os.Stderr, "Syntax Error\n")
 		os.Exit(-1)
@@ -280,84 +284,94 @@ func (i *interpreter) factor() *Node {
 	return node
 }
 
-func (i *interpreter) term() *Node {
-	node := i.factor()
-	for ; prior1(i.Cur().ttype) == true; {
-		token := i.Cur()
+func (r *rules) term() *Node {
+	node := r.factor()
+	for ; prior1(r.lexer.Cur().ttype) == true; {
+		token := r.lexer.Cur()
 		switch token.ttype {
 		case MUL:
-			i.digest(MUL)
+			r.digest(MUL)
 		case DIV:
-			i.digest(DIV)
+			r.digest(DIV)
 		case MOD:
-			i.digest(MOD)
+			r.digest(MOD)
 		}
-		node = &Node{node, token,  i.factor()}
+		node = &Node{node, token,  r.factor()}
 	}
 	return node
 }
 
-func (i *interpreter) expr() *Node {
-	node := i.term()
-	for ; prior2(i.Cur().ttype) == true; {
-		token := i.Cur()
+func (r *rules) expr() *Node {
+	node := r.term()
+	for ; prior2(r.lexer.Cur().ttype) == true; {
+		token := r.lexer.Cur()
 		switch token.ttype {
 		case MINUS:
-			i.digest(MINUS)
+			r.digest(MINUS)
 		case PLUS:
-			i.digest(PLUS)
+			r.digest(PLUS)
 		}
-		node = &Node{node, token,  i.term()}
+		node = &Node{node, token,  r.term()}
 	}
 	return node
 }
 
-func (i *interpreter) assignment_statement() Element {
-	token := i.Cur()
-	i.digest(WORD)
-	variable := Var{token}
-	token = i.Cur()
-	i.digest(ASSIGN)
-	return &Assign{variable, token, i.expr()}
+func (r *rules) variable() Var {
+	token := r.lexer.Cur()
+	r.digest(WORD)
+	return Var{token}
 }
 
-func (i *interpreter) statement() Element {
-	ttype := i.Cur().ttype
+func (r *rules) assignment_statement() Element {
+	variable := r.variable()
+	token := r.lexer.Cur()
+	r.digest(ASSIGN)
+	return &Assign{variable, token, r.expr()}
+}
+
+func (r *rules) statement() Element {
+	ttype := r.lexer.Cur().ttype
 	var node Element
 	if ttype == BEGIN {
-		node = i.compound_statement()
+		node = r.compound_statement()
 	} else if ttype == WORD {
-		node = i.assignment_statement()
+		node = r.assignment_statement()
 	} else {
 		return nil
 	}
 	return node
 }
 
-func (i *interpreter) statement_list() Elem_list {
-	node := i.statement()
+func (r *rules) statement_list() Elem_list {
+	node := r.statement()
 	list := Elem_list{}
 	list.elem = append(list.elem, node)
-	for ; i.Cur().ttype == SEMI ; {
-		i.digest(SEMI)
-		list.elem = append(list.elem, i.statement())
+	for ; r.lexer.Cur().ttype == SEMI ; {
+		r.digest(SEMI)
+		list.elem = append(list.elem, r.statement())
 	}
 	return list
 }
 
-func (i *interpreter) compound_statement() Element {
-	i.digest(BEGIN)
-	node := i.statement_list()
-	i.digest(END)
+func (r *rules) compound_statement() Element {
+	r.digest(BEGIN)
+	node := r.statement_list()
+	r.digest(END)
 	root := Compound{node.elem, make(map[string]int)}
 	return &root
 }
 
-func (i *interpreter) program() Element {
-	node := i.compound_statement()
-	i.digest(DOT)
+func (r *rules) program() Element {
+	r.digest(PROGRAM)
+//	variable := r.variable()
+	r.digest(SEMI)
+
+	node := r.compound_statement()
+	r.digest(DOT)
 	return node
 }
+
+
 
 func run(elem Element, c Compound) int {
 	var result, left, right int
@@ -398,13 +412,13 @@ func run(elem Element, c Compound) int {
 	return result
 }
 
-func (i *interpreter) Parse() {
-	node := i.program()
-	if i.Cur().ttype == EOF {
+func (r *rules) Parse() {
+	node := r.program()
+	if r.lexer.Cur().ttype == EOF {
 		fmt.Println("Parsing FINISHED")
 		node.Resolve()
 	} else {
-		fmt.Fprintf(os.Stderr, "Unexpected token %d '%s'\n", i.Cur().ttype, i.Cur().tvalue)
+		fmt.Fprintf(os.Stderr, "Unexpected token %d '%s'\n", r.lexer.Cur().ttype, r.lexer.Cur().tvalue)
 		os.Exit(-1)
 	}
 }
@@ -449,13 +463,13 @@ func main() {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, data)
 	}
-	tokens := lexer(string(data))
+	tokens := tokenize(string(data))
 
 	reverse_lex := reverse_lex()
 	for i, j := range tokens {
 		fmt.Printf("Token[%d] := {%s} '%s'\n", i, reverse_lex(j.ttype), j.tvalue)
 	}
 
-	interpreter := interpreter{0, len(tokens), tokens}
-	interpreter.Parse()
+	rules := rules{lexer{0, len(tokens), tokens}}
+	rules.Parse()
 }
