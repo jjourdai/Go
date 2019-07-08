32297a7fc9eb92981a0d28cd9f79230a8adb553b
julien <jourdainjulien@gmail.com>
2019-07-08 14:49:42 +0200

    [UPDATE] interpreer

--- part10/main.go
+++ part10/main.go
@@ -14,7 +14,7 @@ const (
 	PLUS = 1
 	MINUS = 2
 	MUL = 3
-	DIV = 4
+	INTEGER_DIV = 4
 	MOD = 5
 	LPAR = 6
 	RPAR = 7
@@ -23,40 +23,55 @@ const (
 	END = 11
 	SEMI = 12
 	ASSIGN = 13
-	WORD = 14
+	ID = 14
 	COLON = 15
 	COMMA = 16
-	REAL = 17
-	INTEGER = 18
+	REAL_CONST = 17
+	INTEGER_CONST = 18
 	EOL = 19
 	EOF = 20
 	VAR = 21
 	PROGRAM = 22
 	OCOMMENT = 23
 	CCOMMENT = 24
+	FLOAT_DIV = 25
 )
 
-/*
-	Interpreter
-*/
+/* STATIC VALUE */
 
-type lexemes struct {
-	ttype int
-	tstring string
+var reverse_lex = map[int]string {
+		PLUS : "PLUS",
+		MINUS : "MINUS",
+		MUL : "MUL",
+		INTEGER_DIV : "INTEGER_DIV",
+		FLOAT_DIV : "FLOAT_DIV",
+		MOD : "MOD",
+		LPAR : "LPAR",
+		RPAR : "RPAR",
+		DOT : "DOT",
+		BEGIN : "BEGIN",
+		END : "END",
+		SEMI : "SEMI",
+		ASSIGN : "ASSIGN",
+		ID : "ID",
+		EOL : "EOL",
+		EOF : "EOF",
+		VAR : "VAR",
+		REAL_CONST : "REAL_CONST",
+		INTEGER_CONST : "INTEGER_CONST",
+		COLON : "COLON",
+		COMMA : "COMMA",
+		PROGRAM : "PROGRAM",
 }
 
-func (n *lexemes) String() string {
-	return fmt.Sprintf("type [%d] value '%s'", n.ttype, n.tstring)
-}
-
-func init_lex() func(key string) int {
-	var lex = map[string]int {
+var lex = map[string]int {
 		"+" : PLUS,
 		"-" : MINUS,
 		"%" : MOD,
 		"*" : MUL,
 		"(" : LPAR,
 		")" : RPAR,
+		"/" : FLOAT_DIV,
 		";" : SEMI,
 		"." : DOT,
 		"," : COMMA,
@@ -64,32 +79,83 @@ func init_lex() func(key string) int {
 		"\n" : EOF,
 		"{" : OCOMMENT,
 		"}" : CCOMMENT,
-	}
-	return func(key string) int {
-		return lex[key]
-	}
 }
 
-func reserved_keyword() func(key string) int {
-	var keyword = map[string]int {
+var keyword = map[string]int {
 		"BEGIN" : BEGIN,
 		"END" : END,
-		"DIV" : DIV,
+		"DIV" : INTEGER_DIV,
 		"VAR" : VAR,
-		"REAL" : REAL,
-		"INTEGER" : INTEGER,
+		"REAL" : REAL_CONST,
+		"INTEGER" : INTEGER_CONST,
 		"PROGRAM" : PROGRAM,
-	}
-	return func(key string) int {
-		return keyword[key]
-	}
+}
+
+/* STRUCT */
+
+type lexemes struct {
+	ttype int
+	tstring string
+}
+
+type lexer struct {
+	index int
+	length int
+	tokens []lexemes
+}
+
+type rules struct {
+	lexer lexer
+}
+
+type Compound struct {
+	elem []interface{}
+}
+
+type Elem_list struct {
+	elem []interface{}
+}
+
+type Spec int
+
+var global_varlist *VarList = nil
+type VarList map[string]*Var
+
+type Var struct {
+	token *lexemes
+	spec *Spec
+	value float64
+}
+
+type Block struct {
+	varlist *VarList
+	compound interface{}
+}
+
+type Assign struct {
+	variable *Var
+	token *lexemes
+	expr *Node
+}
+
+type Node struct {
+	left *Node
+	token *lexemes
+	right *Node
+}
+
+/*
+	LEXER
+*/
+
+func (n *lexemes) String() string {
+	return fmt.Sprintf("type [%d] value '%s'", n.ttype, n.tstring)
 }
 
 func store_new_token(tokens *[]lexemes, new_token **lexemes) {
-	keyword := reserved_keyword()
 	if *new_token != nil {
 		(*new_token).tstring = strings.ToUpper((*new_token).tstring)
-		if kword := keyword((*new_token).tstring); kword != 0 {
+		if kword := keyword[(*new_token).tstring]; kword != 0 {
 			(*new_token).ttype = kword
 		}
 		*tokens = append(*tokens, **new_token)
@@ -98,7 +164,6 @@ func store_new_token(tokens *[]lexemes, new_token **lexemes) {
 }
 
 func tokenize(expr string) []lexemes {
-	lex := init_lex()
 	var tokens []lexemes
 	var new_token *lexemes
 	length := len(expr)
@@ -116,27 +181,27 @@ func tokenize(expr string) []lexemes {
 			continue
 		case expr[index] >= '0' && expr[index] <= '9':
 			if new_token == nil {
-				new_token = &lexemes{INTEGER, ""}
+				new_token = &lexemes{INTEGER_CONST, ""}
 			}
 			new_token.tstring += string(expr[index])
 		case expr[index] >= 65 && expr[index] <= 90 || expr[index] >= 97 && expr[index] <= 122 || expr[index] == '_':
-			if new_token != nil && new_token.ttype != WORD {
+			if new_token != nil && new_token.ttype != ID {
 				store_new_token(&tokens, &new_token)
 			}
 			if new_token == nil {
-				new_token = &lexemes{WORD, ""}
+				new_token = &lexemes{ID, ""}
 			}
 			new_token.tstring += string(expr[index])
 		case expr[index] == ':' && index < length - 1 && expr[index + 1] == '=':
 			store_new_token(&tokens, &new_token)
 			tokens = append(tokens, lexemes{ASSIGN, ":="})
 			index++
-		case expr[index] == '.' && new_token.ttype == INTEGER:
+		case expr[index] == '.' && new_token.ttype == INTEGER_CONST:
 			new_token.tstring += string(expr[index])
-			new_token.ttype = REAL
+			new_token.ttype = REAL_CONST
 		default:
 			store_new_token(&tokens, &new_token)
-			new_val := lex(string(expr[index]))
+			new_val := lex[string(expr[index])]
 			tokens = append(tokens, lexemes{new_val, string(expr[index])})
 			if new_val == 0 {
 				fmt.Fprintf(os.Stderr, "Lexer Error: unexpected character '%c'\n", expr[index])
@@ -149,8 +214,15 @@ func tokenize(expr string) []lexemes {
 	return tokens
 }
 
+/*
+	Parser
+*/
+
 func prior1(current_token int) bool {
-	if current_token == MOD || current_token == DIV || current_token == MUL {
+	if current_token == MOD ||
+		current_token == INTEGER_DIV ||
+		current_token == MUL ||
+		current_token == FLOAT_DIV {
 		return true
 	}
 	return false
@@ -163,118 +235,12 @@ func prior2(current_token int) bool {
 	return false
 }
 
-/*
-	Interpreter
-*/
-
-type lexer struct {
-	index int
-	length int
-	tokens []lexemes
-}
-
-type rules struct {
-	lexer lexer
-}
-
-type Element interface {
-	Resolve()
-}
-
-type Compound struct {
-	elem []Element
-}
-
-func (c Compound) Resolve() {
-	for _, elem := range c.elem {
-		if elem != nil {
-			switch v := elem.(type) {
-			case *Block:
-				fmt.Println("Type Block")
-			case *Compound:
-				v.Resolve()
-			case *Var:
-				fmt.Println("Type Var")
-			case *Assign:
-				fmt.Println("Type Assign")
-				var_name := v.variable.token.tstring
-				_, ok := (*global_varlist)[var_name]
-				if ok == true {
-					(*global_varlist)[var_name].value = run(v.expr)
-				} else {
-					fmt.Fprintf(os.Stderr, "Semantic Error: %s undeclared \n", var_name)
-					os.Exit(-1)
-				}
-				run(v.expr)
-			case *Node:
-				fmt.Println("Type Node")
-			default:
-				fmt.Println("Type unknown")
-			}
-		}
-	}
-}
-
-type Elem_list struct {
-	elem []Element
-}
-
-func (c Elem_list) Resolve() {
-}
-
-type Spec int
-func (c Spec) Resolve() {
-}
-
-var global_varlist *VarList = nil
-type VarList map[string]*Var
-
-func (c *VarList) Resolve() {
-}
-
-type Var struct {
-	token *lexemes
-	spec *Spec
-	value float64
-}
-
 func (v *Var) String() string {
-	return fmt.Sprintf("%v type := %s := %f", v.token, reverse_lex[int(*v.spec)], v.value)
-}
-
-func (c *Var) Resolve() {
-}
-
-type Block struct {
-	varlist *VarList
-	compound Element
-}
-
-func (b *Block) Resolve() {
-	switch b.compound.(type) {
-		case *Compound:
-			b.compound.Resolve()
-		default:
-			fmt.Println("Error")
+	if *v.spec == INTEGER_CONST {
+		return fmt.Sprintf("%v type := %s := %d", v.token, reverse_lex[int(*v.spec)], int64(v.value))
+	} else {
+		return fmt.Sprintf("%v type := %s := %f", v.token, reverse_lex[int(*v.spec)], v.value)
 	}
-}
-
-type Assign struct {
-	variable *Var
-	token *lexemes
-	expr *Node
-}
-
-func (a *Assign) Resolve() {
-}
-
-type Node struct {
-	left *Node
-	token *lexemes
-	right *Node
-}
-
-func (n *Node) Resolve() {
 }
 
 func (l *lexer) Cur() *lexemes {
@@ -309,14 +275,14 @@ func (r *rules) factor() *Node {
 	var node *Node
 	token := r.lexer.Cur()
 	switch token.ttype {
-	case INTEGER:
-		r.digest(INTEGER)
+	case INTEGER_CONST:
+		r.digest(INTEGER_CONST)
 		node = &Node{nil, token, nil}
-	case REAL:
-		r.digest(REAL)
+	case REAL_CONST:
+		r.digest(REAL_CONST)
 		node = &Node{nil, token, nil}
-	case WORD:
-		r.digest(WORD)
+	case ID:
+		r.digest(ID)
 		node = &Node{nil, token, nil}
 	case LPAR:
 		r.digest(LPAR)
@@ -342,10 +308,12 @@ func (r *rules) term() *Node {
 		switch token.ttype {
 		case MUL:
 			r.digest(MUL)
-		case DIV:
-			r.digest(DIV)
-		case MOD:
-			r.digest(MOD)
+		case INTEGER_DIV:
+			r.digest(INTEGER_DIV)
+		case FLOAT_DIV:
+			r.digest(FLOAT_DIV)
+//		case MOD:
+//			r.digest(MOD)
 		}
 		node = &Node{node, token,  r.factor()}
 	}
@@ -369,23 +337,23 @@ func (r *rules) expr() *Node {
 
 func (r *rules) variable() *Var {
 	token := r.lexer.Cur()
-	r.digest(WORD)
+	r.digest(ID)
 	return &Var{token, nil, 0}
 }
 
-func (r *rules) assignment_statement() Element {
+func (r *rules) assignment_statement() interface{} {
 	variable := r.variable()
 	token := r.lexer.Cur()
 	r.digest(ASSIGN)
 	return &Assign{variable, token, r.expr()}
 }
 
-func (r *rules) statement() Element {
+func (r *rules) statement() interface{} {
 	ttype := r.lexer.Cur().ttype
-	var node Element
+	var node interface{}
 	if ttype == BEGIN {
 		node = r.compound_statement()
-	} else if ttype == WORD {
+	} else if ttype == ID {
 		node = r.assignment_statement()
 	} else {
 		return nil
@@ -404,7 +372,7 @@ func (r *rules) statement_list() Elem_list {
 	return list
 }
 
-func (r *rules) compound_statement() Element {
+func (r *rules) compound_statement() interface{} {
 	r.digest(BEGIN)
 	node := r.statement_list()
 	r.digest(END)
@@ -412,15 +380,15 @@ func (r *rules) compound_statement() Element {
 	return &root
 }
 
-func (r *rules) type_spec() Element {
+func (r *rules) type_spec() interface{} {
 	token := r.lexer.Cur()
 	switch token.ttype {
-	case INTEGER:
-		r.digest(INTEGER)
-		return Spec(INTEGER)
-	case REAL:
-		r.digest(REAL)
-		return Spec(REAL)
+	case INTEGER_CONST:
+		r.digest(INTEGER_CONST)
+		return Spec(INTEGER_CONST)
+	case REAL_CONST:
+		r.digest(REAL_CONST)
+		return Spec(REAL_CONST)
 	default:
 		fmt.Fprintf(os.Stderr, "Semantic Error: %s unknown type\n", token.tstring)
 		os.Exit(-1)
@@ -446,7 +414,7 @@ func (r *rules) variable_declaration() Elem_list {
 func (r *rules) declaration() *VarList {
 	r.digest(VAR)
 	varlist := make(VarList)
-	for ; r.lexer.Cur().ttype == WORD; {
+	for ; r.lexer.Cur().ttype == ID; {
 		list := r.variable_declaration()
 		length := len(list.elem)
 		index := 0
@@ -465,20 +433,65 @@ func (r *rules) block() *Block {
 	return &Block{r.declaration(), r.compound_statement()}
 }
 
-func (r *rules) program() Element {
+func (r *rules) program() *Block {
 	r.digest(PROGRAM)
 	r.variable()
 	r.digest(SEMI)
 	node := r.block()
 	global_varlist = node.varlist
+/*
 for index, value := range *node.varlist {
 	fmt.Println(index, value)
 }
+*/
 	r.digest(DOT)
 	return node
 }
 
-func run(elem Element) float64 {
+/*
+	Interpreter
+*/
+
+func (c Compound) Resolve() {
+	for _, elem := range c.elem {
+		if elem != nil {
+			switch v := elem.(type) {
+			case *Block:
+				fmt.Println("Type Block")
+			case *Compound:
+				v.Resolve()
+			case *Var:
+				fmt.Println("Type Var")
+			case *Assign:
+				fmt.Println("Type Assign")
+				var_name := v.variable.token.tstring
+				_, ok := (*global_varlist)[var_name]
+				if ok == true {
+					(*global_varlist)[var_name].value = run(v.expr)
+				} else {
+					fmt.Fprintf(os.Stderr, "Semantic Error: %s undeclared \n", var_name)
+					os.Exit(-1)
+				}
+				run(v.expr)
+			case *Node:
+				fmt.Println("Type Node")
+			default:
+				fmt.Println("Type unknown")
+			}
+		}
+	}
+}
+
+func (b *Block) Resolve() {
+	switch l := b.compound.(type) {
+		case *Compound:
+			l.Resolve()
+		default:
+			fmt.Println("Error")
+	}
+}
+
+func run(elem interface{}) float64 {
 	var result, left, right float64
 	var test *Node
 	test, ok := elem.(*Node)
@@ -497,18 +510,20 @@ func run(elem Element) float64 {
 			result = left - right
 		case PLUS:
 			result = left + right
-		case DIV:
+		case INTEGER_DIV:
+			result = left / right
+		case FLOAT_DIV:
 			result = left / right
 		case MUL:
 			result = left * right
 //		case MOD:
 //			result = left % right
-		case INTEGER:
+		case INTEGER_CONST:
 			tmp, _ := strconv.ParseInt(test.token.tstring, 10, 64)
 			result = float64(tmp)
-		case REAL:
+		case REAL_CONST:
 			result, _ = strconv.ParseFloat(test.token.tstring, 64)
-		case WORD:
+		case ID:
 			var_name := test.token.tstring
 			_, ok := (*global_varlist)[var_name]
 	//		fmt.Println(var_name)
@@ -535,30 +550,6 @@ func (r *rules) Parse() {
 		fmt.Fprintf(os.Stderr, "Unexpected token %d '%s'\n", r.lexer.Cur().ttype, r.lexer.Cur().tstring)
 		os.Exit(-1)
 	}
-}
-
-var reverse_lex = map[int]string {
-		PLUS : "PLUS",
-		MINUS : "MINUS",
-		MUL : "MUL",
-		DIV : "DIV",
-		MOD : "MOD",
-		LPAR : "LPAR",
-		RPAR : "RPAR",
-		DOT : "DOT",
-		BEGIN : "BEGIN",
-		END : "END",
-		SEMI : "SEMI",
-		ASSIGN : "ASSIGN",
-		WORD : "WORD",
-		EOL : "EOL",
-		EOF : "EOF",
-		VAR : "VAR",
-		REAL : "REAL",
-		INTEGER : "INTEGER",
-		COLON : "COLON",
-		COMMA : "COMMA",
-		PROGRAM : "PROGRAM",
 }
 
 func main() {
